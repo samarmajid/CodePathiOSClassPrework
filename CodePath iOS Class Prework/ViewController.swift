acec440fb52dce7f57723c103c60078377b2866c
Samar Majid <summermajid@gmail.com>
2022-02-05 00:53:52 -0500

    init

---
+++ CodePath iOS Class Prework/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  CodePath iOS Class Prework
+//
+//  Created by Samar Majid on 2/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var billAmountTextField: UITextField!
+    
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        // Get bill amount from text field input
+        let bill = Double(billAmountTextField.text!) ?? 0
+        
+        // Get Total tip by multiplying tip * tipPercentage
+        let tipPercentages = [0.15, 0.18, 0.2]
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        // Update Tip Amount Label
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        
+        // Update total amount label
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+}
+
